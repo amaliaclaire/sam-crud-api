62836a607e913e7354adc8fb829ebbf38eb93ea5
Amalia Claire Fergin <acfergin@msn.com>
2018-02-19 13:55:25 -0800

    migration, seeds, all working'

--- knexfile.js
+++ knexfile.js
@@ -1,14 +1,25 @@
 const path = require('path')
+require('dotenv').load()
 
 module.exports = {
-  development: {
-    client: 'pg',
-    connection: 'postgres://localhost:5432/museum_db_dev',
-    migrations: {
-      directory: path.join(__dirname, 'db', 'migrations')
-    },
-    seeds: {
-      directory: path.join(__dirname, 'db', 'seeds')
+    development: {
+      client: 'pg',
+      connection: `postgres://localhost:5432/${process.env.DATABASE_NAME}`,
+      migrations: {
+        directory: path.join(__dirname, 'db', 'migrations')
+      },
+      seeds: {
+        directory: path.join(__dirname, 'db', 'seeds')
+      },
+      production: {
+        client: 'pg',
+        connection: process.env.DATABASE_URL,
+        migrations: {
+          directory: path.join(__dirname, 'db', 'migrations')
+        },
+        seeds: {
+          directory: path.join(__dirname, 'db', 'seeds')
+        }
     }
   }
-};
+}
